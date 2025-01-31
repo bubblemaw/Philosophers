a8e020392ee3f32f833d8f8ed1db3c5f895685f9
Maw <mawuli.asase@gmail.com>
2025-01-31 10:14:46 +0100

    taulard

--- main.c
+++ main.c
@@ -1,27 +1,14 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   philo.h                                            :+:      :+:    :+:   */
+/*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: maw <maw@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/01/28 16:50:25 by maw               #+#    #+#             */
-/*   Updated: 2025/01/30 19:00:49 by maw              ###   ########.fr       */
+/*   Created: 2025/01/30 17:39:00 by maw               #+#    #+#             */
+/*   Updated: 2025/01/30 19:00:51 by maw              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PHILO_H
-# define PHILO_H
+#include "philo.h"
 
-# include <unistd.h>
-# include <stdlib.h>
-# include <stdio.h>
-# include <pthread.h>
-
-typedef struct s_philo
-{
-	pthread_t	thread;
-
-}	t_philo;
-
-#endif
