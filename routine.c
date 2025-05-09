010d6a202439744f6793dc8d37e5543325010519
Mawuli-Kplim Asase <masase@c3r2s2.42lausanne.ch>
2025-05-09 13:21:24 +0200

    uoi

--- routine.c
+++ routine.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   routine.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: maw <maw@student.42.fr>                    +#+  +:+       +#+        */
+/*   By: masase <masase@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/31 11:58:56 by masase            #+#    #+#             */
-/*   Updated: 2025/05/08 22:16:16 by maw              ###   ########.fr       */
+/*   Updated: 2025/05/09 12:30:50 by masase           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -56,12 +56,16 @@ int	eating(t_philo *philo)
 		current_time = get_time();
 		// printf("%ld\n", current_time - start_time);
 		if (philo->monitor->dead == 1)
+		{
+			pthread_mutex_unlock(philo->left_fork_mutex);
+			pthread_mutex_unlock(philo->right_fork_mutex);
 			return (0);
+		}
 	}
-	// pthread_mutex_lock(&philo->monitor->eating_mutex);
+	pthread_mutex_lock(&philo->monitor->eating_mutex);
 	philo->last_meal = get_time();
 	philo->eating = 0;
-	// pthread_mutex_unlock(&philo->monitor->eating_mutex);
+	pthread_mutex_unlock(&philo->monitor->eating_mutex);
 	philo->meals_done++;
 	philo->think_flag = 0;
 	pthread_mutex_unlock(philo->left_fork_mutex);
@@ -78,6 +82,8 @@ void    *routine(void *arg)
 	{
 		// if (philo->think_flag == 0)
 			think(philo);
+			if (philo->id % 2 == 0)
+				usleep(1000);
 		// if (philo->left_fork_mutex)
 		// {
 			pthread_mutex_lock(philo->left_fork_mutex);
@@ -169,6 +175,7 @@ void	*monitor_routine(void *arg)
 {
 	int			i;
 	int			num_philo;
+	size_t		current_time;
 	t_monitor	*monitor;
 	int			meals_done_flag;
 
@@ -179,13 +186,15 @@ void	*monitor_routine(void *arg)
 	while (1)
 	{
 		i = 0;
+		usleep(500);
 		while (i < num_philo)
 		{
-			// pthread_mutex_lock(&monitor->eating_mutex);
-			if (get_time() - monitor->philo[i].last_meal
-				>= monitor->philo[i].time_to_die
+			pthread_mutex_lock(&monitor->eating_mutex);
+			if (current_time - monitor->philo[i].last_meal
+				> monitor->philo[i].time_to_die
 				&& monitor->philo[i].eating == 0)
 			{
+				printf("%ld\n", current_time - monitor->philo[i].last_meal);
 				monitor->philo[i].dead = 1;
 				monitor->dead = 1;
 				pthread_mutex_unlock(monitor->philo[i].left_fork_mutex);
@@ -193,16 +202,17 @@ void	*monitor_routine(void *arg)
 				// printf("%ld %d died\n",
 				// 	(get_time() - monitor->simu_start), monitor->philo[i].id);
 				// printf("c'est monitor qui fait quitter\n");
-				// pthread_mutex_unlock(&monitor->eating_mutex);
+				pthread_mutex_unlock(&monitor->eating_mutex);
 				return (NULL);
 			}
-			// pthread_mutex_unlock(&monitor->eating_mutex);
+			pthread_mutex_unlock(&monitor->eating_mutex);
 			if (monitor->meals_counter_flag == 1
 				&& meals_manager(monitor, &meals_done_flag, i) == 0)
 			{
 				monitor->dead = 1;
 				return (NULL);
 			}
+			current_time = get_time();
 			i++;
 		}
 	}
